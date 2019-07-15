440f04c67e520b507102214b392651fa7b4e7bdd
Sofien MNASSRI <soofinco.m@gmail.com>
2019-07-15 22:16:49 +0200

    first commit

---
+++ api/server/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  });
+  return User;
+};
